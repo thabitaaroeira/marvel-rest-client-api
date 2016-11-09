69039f7fcca32e18128c8bd525d8f37aa8d2fabc
Thabita Aroeira <thabitaaroeira@gmail.com>
2016-11-09 20:02:54 -0200

    Initial commit

---
+++ marvel-client/src/main/java/br/com/thabita/MarvelClientApplication.java
@@ -0,0 +1,12 @@
+package br.com.thabita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelClientApplication.class, args);
+	}
+}
