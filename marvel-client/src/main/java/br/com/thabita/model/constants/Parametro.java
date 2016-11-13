f950c06e6afb13d6089c6736a0381477ceb3937f
Thabita Aroeira <thabitaaroeira@gmail.com>
2016-11-13 12:17:34 -0200

    Javadoc de Parametro

--- marvel-client/src/main/java/br/com/thabita/model/constants/Parametro.java
+++ marvel-client/src/main/java/br/com/thabita/model/constants/Parametro.java
@@ -4,82 +4,61 @@ package br.com.thabita.model.constants;
  * Parametros utilizados na Query.
  */
 public enum Parametro {
-	
-	//@formatter:off
-    NAME("name"),
-    NAME_STARTS_WITH("nameStartsWith"),
-    MODIFIED_SINCE("modifiedSince"),
 
-    FIRST_NAME("firstName"),
-    MIDDLE_NAME("middleName"),
-    LAST_NAME("lastName"),
-    SUFFIX("suffix"),
-    FIRST_NAME_STARTS_WITH("firstNameStartsWith"),
-    MIDDLE_NAME_STARTS_WITH("middleNameStartsWith"),
-    LAST_NAME_STARTS_WITH("lastNameStartsWith"),
+	NAME("name"), NAME_STARTS_WITH("nameStartsWith"), MODIFIED_SINCE("modifiedSince"),
 
-    COMICS("comics"),
-    CREATORS("creators"),
-    EVENTS("events"),
-    SERIES("series"),
-    STORIES("stories"),
-    /**
-     * Values can be assigned from {@link com.pcab.marvel.model.common.OrderBy}
-     */
-    ORDER_BY("orderBy"),
-    TITLE("title"),
-    TITLE_STARTS_WITH("titleStartsWith"),
-    /**
-     * Values can be assigned from {@link com.pcab.marvel.model.comic.Format}
-     */
-    FORMAT("format"),
-    /**
-     * Values can be assigned from {@link com.pcab.marvel.model.comic.FormatType}
-     */
-    FORMAT_TYPE("formatType"),
-    /**
-     * Only accepts TRUE boolean value
-     */
-    NO_VARIANTES("noVariants"),
-    ISSUE_NUMBER("issueNumber"),
-    DIAMOND_CODE("diamondCode"),
-    DIGITAL_ID("digitalId"),
-    UPC("upc"),
-    ISBN("isbn"),
-    EAN("ean"),
-    ISSN("issn"),
-    /**
-     * Only accepts TRUE boolean value
-     */
-    HAS_DIGITAL_ISSUE("hasDigitalIssue"),
-    SHARED_APPEARANCES("sharedAppearances"),
-    COLLABORATORS("collaborators"),
-    SERIES_TYPE("seriesType"),
-    CONTAINS("contains"),
-    /**
-     * Values can be assigned from {@link com.pcab.marvel.model.common.DateDescriptor}
-     */
+	FIRST_NAME("firstName"), MIDDLE_NAME("middleName"), LAST_NAME("lastName"), SUFFIX("suffix"), FIRST_NAME_STARTS_WITH(
+			"firstNameStartsWith"), MIDDLE_NAME_STARTS_WITH(
+					"middleNameStartsWith"), LAST_NAME_STARTS_WITH("lastNameStartsWith"),
 
-    DATE_DESCRIPTOR("dateDescriptor"),
-    DATE_RANGE("dateRange"),
-    START_YEAR("startYear"),
-    OFFSET("offset"),
-    LIMIT("limit");
-	//@formatter:on
+	/**
+	 * Entidades possiveis de retorno:
+	 */
+	COMICS("comics"), CREATORS("creators"), EVENTS("events"), SERIES("series"), STORIES("stories"),
 
-    private Parametro(String name){
-        this.name = name;
-    }
+	/**
+	 * Valores podem ser preenchidos a partir de
+	 * {@link br.com.thabita.model.constantes.Ordenacao}
+	 */
+	ORDER_BY("orderBy"), TITLE("title"), TITLE_STARTS_WITH("titleStartsWith"),
 
-    private String name;
+	/**
+	 * Valores podem ser: comic, magazine, trade paperback, hardcover, digest,
+	 * graphic novel, digital comic, infinite comic.
+	 */
+	FORMAT("format"),
 
-    /**
-     * Retrieves the string query parameter name.
-     *
-     * @return String representation of the name of the query parameter.
-     */
-    public String getName(){
-        return name;
-    }
+	/**
+	 * Valores podem ser: comic, collection.
+	 */
+	FORMAT_TYPE("formatType"),
+
+	/**
+	 * Aceitam apenas valor boolean TRUE.
+	 */
+	NO_VARIANTES("noVariants"), ISSUE_NUMBER("issueNumber"), DIAMOND_CODE("diamondCode"), DIGITAL_ID("digitalId"), UPC("upc"), ISBN("isbn"), EAN("ean"), ISSN("issn"),
+
+	/**
+	 * Aceitam apenas valor boolean TRUE.
+	 */
+	HAS_DIGITAL_ISSUE("hasDigitalIssue"), SHARED_APPEARANCES("sharedAppearances"), COLLABORATORS("collaborators"), SERIES_TYPE("seriesType"), CONTAINS("contains"),
+
+	/**
+	 * Valores podem ser: thisWeek, lastWeek, nextWeek, thisMonth.
+	 */
+	DATE_DESCRIPTOR("dateDescriptor"), DATE_RANGE("dateRange"), START_YEAR("startYear"), OFFSET("offset"), LIMIT("limit");
+
+	private Parametro(String name) {
+		this.name = name;
+	}
+
+	private String name;
+
+	/**
+	 * @return nome do Parametro
+	 */
+	public String getName() {
+		return name;
+	}
 
 }
